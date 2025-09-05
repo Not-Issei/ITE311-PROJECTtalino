70accdb4cae1af08052104e11a39fafcf2964ce1
Not-Issei <talinokrish@gmail.com>
2025-09-05 14:55:36 +0800

    Added Bootstrao template and Home Controller

---
+++ app/Views/home.php
@@ -0,0 +1,5 @@
+<?= $this->extend('template') ?>
+<?= $this->section('content') ?>
+<h1>Welcome to ITE311 Home Page</h1>
+<p>This page is powered by CodeIgniter + Bootstrap.</p>
+<?= $this->endSection() ?>
